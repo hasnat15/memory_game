706662cbb39c9076f61d41679a24e42233bc9c52
Muhammad Hasnat Umar <mhasnat749@gmail.com>
2025-02-05 15:43:16 +0000

    fetch data from API

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,24 @@ import MemoryCard from './components/MemoryCard'
 export default function App() {
     const [isGameOn, setIsGameOn] = useState(false)
     
-    function startGame(e) {
-        e.preventDefault()
-        setIsGameOn(true)
-    }
+    async function startGame(e) {
+      e.preventDefault()
+      
+      try {
+          const response = await fetch("https://emojihub.yurace.pro/api/all/category/animals-and-nature")
+          
+          if (!response.ok) {
+              throw new Error("Could not fetch data from API")
+          }
+          
+          const data = await response.json()
+          console.log(data)
+          setIsGameOn(true) 
+      } catch(err) {
+          console.error(err)
+      }
+      
+  }
     
     function turnCard() {
         console.log("Memory card clicked")
